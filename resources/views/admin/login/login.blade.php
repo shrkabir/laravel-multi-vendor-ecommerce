b88d9ac8741f2ddf8de62b9dabbfd2ad6b2d0ea6
Ahmed Shahriar Kabir <sazzad.shrkabir@gmail.com>
2023-02-12 23:11:42 +0600

    login page assets url/src modified.

--- resources/views/admin/login/login.blade.php
+++ resources/views/admin/login/login.blade.php
@@ -7,16 +7,16 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <title>Skydash Admin</title>
   <!-- plugins:css -->
-  <link rel="stylesheet" href="../../vendors/feather/feather.css">
-  <link rel="stylesheet" href="../../vendors/ti-icons/css/themify-icons.css">
-  <link rel="stylesheet" href="../../vendors/css/vendor.bundle.base.css">
+  <link rel="stylesheet" href="{{url('admin')}}/vendors/feather/feather.css">
+  <link rel="stylesheet" href="{{url('admin')}}/vendors/ti-icons/css/themify-icons.css">
+  <link rel="stylesheet" href="{{url('admin')}}/vendors/css/vendor.bundle.base.css">
   <!-- endinject -->
   <!-- Plugin css for this page -->
   <!-- End plugin css for this page -->
   <!-- inject:css -->
-  <link rel="stylesheet" href="../../css/vertical-layout-light/style.css">
+  <link rel="stylesheet" href="{{url('admin')}}/css/vertical-layout-light/style.css">
   <!-- endinject -->
-  <link rel="shortcut icon" href="../../images/favicon.png" />
+  <link rel="shortcut icon" href="{{url('admin')}}/images/favicon.png" />
 </head>
 
 <body>
@@ -27,7 +27,7 @@
           <div class="col-lg-4 mx-auto">
             <div class="auth-form-light text-left py-5 px-4 px-sm-5">
               <div class="brand-logo">
-                <img src="../../images/logo.svg" alt="logo">
+                <img src="{{url('admin')}}/images/logo.svg" alt="logo">
               </div>
               <h4>Hello! let's get started</h4>
               <h6 class="font-weight-light">Sign in to continue.</h6>
@@ -69,16 +69,16 @@
   </div>
   <!-- container-scroller -->
   <!-- plugins:js -->
-  <script src="../../vendors/js/vendor.bundle.base.js"></script>
+  <script src="{{url('admin')}}/vendors/js/vendor.bundle.base.js"></script>
   <!-- endinject -->
   <!-- Plugin js for this page -->
   <!-- End plugin js for this page -->
   <!-- inject:js -->
-  <script src="../../js/off-canvas.js"></script>
-  <script src="../../js/hoverable-collapse.js"></script>
-  <script src="../../js/template.js"></script>
-  <script src="../../js/settings.js"></script>
-  <script src="../../js/todolist.js"></script>
+  <script src="{{url('admin')}}/js/off-canvas.js"></script>
+  <script src="{{url('admin')}}/js/hoverable-collapse.js"></script>
+  <script src="{{url('admin')}}/js/template.js"></script>
+  <script src="{{url('admin')}}/js/settings.js"></script>
+  <script src="{{url('admin')}}/js/todolist.js"></script>
   <!-- endinject -->
 </body>
 
