f87b1c22de9e7298bdb9d4277e012e6a280550f6
Ahmed Shahriar Kabir <sazzad.shrkabir@gmail.com>
2023-05-05 23:08:06 +0600

    data added in vendor business details seeder and vendor business details seeder added in database seeder.

--- database/seeders/VendorBusinessDetailsSeeder.php
+++ database/seeders/VendorBusinessDetailsSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Admin\Vendor\VendorBusinessDetails;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class VendorBusinessDetailsSeeder extends Seeder
 {
@@ -14,6 +15,22 @@ class VendorBusinessDetailsSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $vendorBusinessDetails= [
+            'id' => 1,
+            'vendor_id' => 1,
+            'shop_name' => 'Abc Shop',
+            'shop_address' => '1/3, Dhaka-0025',
+            'shop_country_id' => 1,
+            'shop_state_id' => 1,
+            'shop_city_id' => 1,
+            'shop_pincode' => '1151165',
+            'shop_phone' => '02056516515',
+            'shop_website' => 'www.shopsite.com',
+            'shop_email' => 'abcshop@mail.com',
+            'shop_licence_number' => '1515165165'
+
+        ];
+
+        VendorBusinessDetails::insert($vendorBusinessDetails);
     }
 }
