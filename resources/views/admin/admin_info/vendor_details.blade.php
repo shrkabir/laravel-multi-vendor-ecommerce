e99d2095bce292d6d89f530d8ab28f41796bb594
Ahmed Shahriar Kabir <sazzad.shrkabir@gmail.com>
2023-12-22 15:57:27 +0600

    vendor details page design

--- resources/views/admin/admin_info/vendor_details.blade.php
+++ resources/views/admin/admin_info/vendor_details.blade.php
@@ -15,7 +15,15 @@
                                 <img src="{{asset('admin/images/photos')}}/img.jpg" alt="" class="rounded-circle">
                             </div>
                             <div class="col-6">
-                                <h3>{{$vendorDetails->name}}</h3>
+                                <h3>{{$vendorDetails->name}}
+                                    <sup>
+                                        @if($vendorDetails->status == 1)
+                                        <span class="badge badge-pill badge-success ml-2">Active</span>
+                                        @elseif($vendorDetails->status == 0)
+                                        <span class="badge badge-pill badge-danger ml-2">Inactive</span>
+                                        @endif
+                                    </sup>
+                                </h3>
                                 <h4>{{$vendorDetails->email}}</h4>
                                 <h4>{{$vendorDetails->mobile}}</h4>
                             </div>
@@ -25,6 +33,11 @@
             </div>
         </div>
     </div>
+    <!-- <div class="row">
+        <div class="col-md-12 grid-margin stretch-card">
+            
+        </div>
+    </div> -->
 </div>
 
 @push('script')
